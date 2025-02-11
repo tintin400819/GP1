9e1114dadf2c26cb0c4d37e5a479961d74ed6753
tintin400819 <tintin.400819@gmail.com>
2025-02-11 10:07:41 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -2,7 +2,7 @@
 # SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
 
 # Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7102257066:AAHJ7ssWRJtlbb6oKhSf61iHsZ9Gh6Gn-vM")
+BOT_TOKEN=("7830511858:AAEODO9pp_OeLh2rETh4O6UZ3eND2fZt0QM")
 
 # Enter Your telegram usernames here without @
 OWNER_USERNAME=("@Ayush143", "@ClutchGodOp")
